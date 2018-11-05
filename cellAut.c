fc895c6e23b582404c26a8ecf005099ba85c9541
Esther Bolik <estherbolik@gmail.com>
2018-11-05 05:49:50 +0000

    Add Game of Life funtion, improve menu
    I have yet to write the interface/menu for the Life function.
    The setup function is also required for the program to work...

--- cellAut.c
+++ cellAut.c
@@ -13,14 +13,39 @@
 
 #include "cellAut.h"
 
-// global settings variables for the current automaton
-unsigned char rule; // The current rule (0..255)
-char **output = NULL; // The output of the automaton, as a 2-dimensional array
-size_t rows, columns; // The dimensions of the output array
-bool xWrap; // Whether or not to wrap the automaton on the x axis (if false, OOB parents will be treated as 0)
-char cTrue, cFalse; // Characters to use when displaying the automaton's output
+////////////////////////////////////////////////////////////////////////////////
+
+/* global variables for the current automaton */
+
+// The current rule (0..255). Only used for 1D automata.
+unsigned char rule = 0;
+
+// The output of the automaton, as a 2-dimensional array.
+// Note that for 1D automata, each row of this array is an individual
+// generation, advancing downwards. On the other hand, for the Game of Life, the
+// array represents the entire current generation (and only the current
+// generation).
+char **output = NULL;
+
+// The dimensions of the output array.
+// rows represents rows of cells in the Game of Life, but represents generations
+// in 1D automata. columns always represents columns of cells.
+size_t rows = 1;
+size_t columns = 1;
+
+// Whether or not to wrap the automaton on the x axis (if false, OOB parents will be treated as 0)
+bool xWrap = false;
+// Whether or not to wrap on the y axis (ONLY USED FOR GAME OF LIFE)
+bool yWrap = false;
+
+// Characters to use when displaying the automaton's output
+char cTrue = 'X';
+char cFalse = ' ';
 
 ////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+// note: all function descriptions are located in cellAut.h
 
 int main(int argc, char const *argv[]) {
   rule = 30;
@@ -33,7 +58,15 @@ int main(int argc, char const *argv[]) {
   cTrue = 'X';
   cFalse = ' ';
 
-  runAutomaton(true);
+  //runAutomaton(true);
+  freeOutput();
+
+  rows = 3;
+  columns = 3;
+  initOutput();
+  printOutput();
+  runGameOfLife(1);
+  printOutput();
   freeOutput();
 
   menu();
@@ -41,69 +74,72 @@ int main(int argc, char const *argv[]) {
 }
 
 void menu() {
+  while (true) { // loop until the function is returned from
+    printf(" [1] Run 1D automaton\n");
+    printf(" [2] Run Game of Life\n");
+    //printf(" [3] Load automaton\n");
+    //printf(" [4] Save the most recent automaton to a file:\n");
+    //printf(" [5] Print out current automaton\n");
+    printf(" [0] Exit program\n");
 
-  char *input;
-  printf("Please select and option from below:\n");
+    bool incorrectInput = false;
+    do {
+      printf("Please enter a number: ");
+      int input;
+      clearBuffer();
+      int status = scanf("%d", &input);
+      if (status <= 0) { // ie. they did not enter a number
+        continue;
+      }
 
-  //set the output to NULL, so we know if the user actually has an automaton currently set up
-  output = NULL;
-  rows = columns = 0;
+      switch (input) {
+        case 1:
+          // TODO: function for changing settings goes here
+          runAutomaton(true);
+          // TODO: prompt to save
+          break;
 
-  printf("\n\nType 1 to Change the settings for the Automaton:\n");
-  printf("Type 2 to run the Cellular Automaton:\n");
-  printf("Type 3 to Load an Automaton from a file:\n");
-  printf("Type 4 to save the most recent Automaton to a file:\n");
-  printf("Type 5 to Print out current automaton\n");
-  printf("Type 0 to exit:\n");
+        case 2:
+          clearBuffer();
+          // TODO: function for changing settings goes here
+          // TODO: enter game of life menu
+          break;
+/*
+        case 3:
+          clearBuffer();
+          output = loadFile();
+          //if automaton hasn't yet been initialised
+          if (output == NULL) {
+            printf("Error encountered\n");
+          }
+          break;
 
-  //get input from the getInput function
-  while (*(input = getInput(2)) != '0') {
+        case 4:
+          clearBuffer();
+          if (!saveFile()) {
+            printf("Error encountered\n");
+          }
+          break;
 
-    switch (*input) {
-      case '1':
-        clearBuffer(input);
-        //function for changing settings goes here
-        break;
-      case '2':
-        clearBuffer(input);
-        //function to run the Automaton
-        break;
-      case '3':
-        clearBuffer(input);
-        output = loadFile();
-        //if automaton hasn't yet been initialised
-        if (output == NULL) {
-          printf("Error encountered\n");
-        }
-        break;
-      case '4':
-        clearBuffer(input);
-        if (!saveFile()) {
-          printf("Error encountered\n");
-        }
-        break;
-      case '5':
-        clearBuffer(input);
-        if(!printOutput()) {
-          printf("Error Encountered\n");
-        }
-        break;
-      default:
-        printf("Unkown input %s, please try again: \n\n", input);
-        break;
-    }
-
-    printf("\n\nType 1 to Change the settings for the Automaton:\n");
-    printf("Type 2 to run the Cellular Automaton:\n");
-    printf("Type 3 to Load an Automaton from a file:\n");
-    printf("Type 4 to save the most recent Automaton to a file:\n");
-    printf("Type 5 to Print out current automaton\n");
-    printf("Type 0 to exit:\n");
+        case 5:
+          clearBuffer();
+          if(!printOutput()) {
+            printf("Error Encountered\n");
+          }
+          break;
+*/
+        default:
+          printf("'%d' is not a valid choice. ", input);
+          incorrectInput = true;
+          break;
+      }
+    } while (incorrectInput);
   }
 
   freeOutput();
 }
 
+////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
 void initOutput() {
@@ -154,6 +190,9 @@ int printOutput() {
   return 1;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
 int runAutomaton(bool printOutput) {
   if (output == NULL) {
     fprintf(stderr, "runAutomaton: Output array has not been initialised\n");
@@ -166,7 +205,7 @@ int runAutomaton(bool printOutput) {
     for (size_t col = 0; col < columns; col++) { // for each cell in the row
       char lParent;
       if (col > 0) {
-        lParent = (output[row - 1][col - 1]);
+        lParent = output[row - 1][col - 1];
       } else if (xWrap) {
         lParent = output[row - 1][columns - 1];
       } else {
@@ -201,11 +240,112 @@ int runAutomaton(bool printOutput) {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-/*
-This function attempts to open the given file in read or write mode
-The function takes in the file name and the mode and will return either
-the pointer to the file or NULL if there was an error
-*/
+int runGameOfLife(int generations) {
+  if (output == NULL) {
+    fprintf(stderr, "runGameOfLife: Output array has not been initialised\n");
+  }
+
+  // gen 0 is the first generation; gen is incremented at the end of each
+  // generation, eg. the step from gen 0 to 1 will increment 'gen' from 0 to 1
+  // at the *end* of the loop.
+  for (int gen = 0; gen < generations; gen++) {
+    // move the previous generation to a separate variable and make a new 2D
+    // array to store the new generation
+    char **parent = output; // 2D char array for storing the previous state of the board
+    initOutput(); // convert the "output" pointer to a new 2D array
+
+    for (size_t row = 0; row < rows; row++) { // for every row
+      for (size_t col = 0; col < columns; col++) { // for every cell of that row
+        // sum of the values of the 8 neighbours in the parent generation
+        char parentSum = 0;
+
+        // upper row
+        if (row > 0 || yWrap) {
+          char *uRow; // the row that we're treating as being immediately above this one
+          if (row > 0) { // ie. there is a row above the current one
+            uRow = parent[row - 1];
+          } else if (yWrap) {
+            uRow = parent[rows - 1]; // bottom row
+          }
+
+          // upper-left cell
+          if (col > 0) { // ie. there is a column to the left of the current one
+            parentSum += uRow[col - 1];
+          } else if (xWrap) {
+            parentSum += uRow[columns - 1]; // rightmost cell of uRow
+          }
+          // upper-middle cell
+          parentSum += uRow[col]; // the current col evidently exists
+          // upper-right cell
+          if (col + 1 < columns) { // ie. there is a column to the right of the current one
+            parentSum += uRow[col + 1];
+          } else if (xWrap) {
+            parentSum += uRow[0]; // leftmost cell of uRow
+          }
+        }
+
+        // center-left cell
+        if (col > 0) { // ie. there is a column to the left of the current one
+          parentSum += parent[row][col - 1];
+        } else if (xWrap) {
+          parentSum += parent[row][columns - 1];
+        }
+        // center-right cell
+        if (col + 1 < columns) {
+          parentSum += parent[row][col + 1];
+        } else if (xWrap) {
+          parentSum += parent[row][0];
+        }
+
+        // lower row
+        if (row + 1 < rows || yWrap) {
+          char *dRow;
+          if (row + 1 < rows) { // ie. there is a row below the current one
+            dRow = parent[row + 1];
+          } else if (yWrap) {
+            dRow = parent[0]; // top row
+          }
+
+          // lower-left cell
+          if (col > 0) {
+            parentSum += dRow[col - 1];
+          } else if (xWrap) {
+            parentSum += dRow[columns - 1]; // rightmost cell of dRow
+          }
+          // lower-middle cell
+          parentSum += dRow[col];
+          // lower-right cell
+          if (col + 1 < columns) {
+            parentSum += dRow[col + 1];
+          } else if (xWrap) {
+            parentSum += dRow[0]; // leftmost cell of dRow
+          }
+        }
+
+        // Calculate value of current cell according to the rules of Life. It is
+        // assumed that each cell holds either the value '0' (dead) or '1'
+        // (alive).
+        if (parentSum < 2 || parentSum > 3) {
+          output[row][col] = 0;
+        } else { // ie. 2 <= parentSum <= 3
+          output[row][col] = 1;
+        }
+      } // next col
+    } // next row
+
+    // free the parent generation
+    for (size_t i = 0; i < rows; i++) {
+      free(parent[i]);
+    }
+    free(parent);
+
+  } // next gen
+  return 1;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
 FILE *attemptOpen(char* fileName, char* mode) {
 
   FILE *pFile;
@@ -213,33 +353,29 @@ FILE *attemptOpen(char* fileName, char* mode) {
   pFile = fopen(fileName, mode);
 
   if (pFile == NULL) {
-    fprintf(stderr, "The file '%s' does not exist.\n", fileName);
+    fprintf(stderr, "attemptOpen: The file '%s' does not exist.\n", fileName);
   }
-
-  free(fileName);
 
   return pFile;
 }
 
-/*
-The following function saves the array to a file and then returns 0 if a error
-was encountered or 1 if it was successful. The parameters are the array to be
-written to file and the numbers of rows and columns the array has
-*/
 int saveFile() {
 
   if (output == NULL) {
-    printf("Cannot save NULL output\n");
+    printf("saveFile: Cannot save NULL output\n");
     return 0;
   }
 
-  printf("Please enter in the file name to be saved\n");
-  char *fileName = getInput(256);
+  printf("Please enter the filename to save to: ");
+  char *fileName = NULL;
+  getInput(&fileName, NULL);
+
   //Open file in write mode
   FILE *pFile = attemptOpen(fileName, "w");
+  free(fileName);
 
   if (pFile == NULL) {
-    printf("Malloc ran out of Memory\n");
+    printf("saveFile: Out of memory\n");
     return 0;
   }
 
@@ -252,26 +388,22 @@ int saveFile() {
     //put a newline to signify the end of the line, used when reading
     fputc('\n', pFile);
   }
+
   //close file
   fclose(pFile);
 
   return 1;
 }
 
-/*
-The following function loads a file using the fopen function, It then Prompts the user
-for a filename to open, and returns a 2d pointer array to what was found within the file
-or will return NULL if an error was encountered.
-
-Also takes in pointers to the rows and columns, so that they can be changed to the
-correct values
-*/
 char **loadFile() {
 
-  printf("Please enter in the file name to be loaded\n");
-  char *fileName = getInput(256);
+  printf("Please enter the filename to load from: ");
+  char *fileName = NULL;
+  getInput(&fileName, NULL);
+
   //open the file in read mode
   FILE *pFile = attemptOpen(fileName, "r");
+  free(fileName);
 
   if (pFile == NULL) {
     return NULL;
@@ -292,14 +424,12 @@ char **loadFile() {
   }
 
   rows = c;
-  //don't need to cast to a float as this will always be a whole number
   columns = i/c;
 
   char** boardToLoad = malloc((rows) * sizeof(char *));
 
   if (boardToLoad == NULL) {
-
-    printf("Malloc fail, out of memory\n");
+    fprintf(stderr, "loadFile: Out of memory error when trying to allocate rows\n");
     return NULL;
   }
 
@@ -309,7 +439,7 @@ char **loadFile() {
 
     if (boardToLoad[i] == NULL) {
 
-      printf("Malloc fail, out of memory\n");
+      fprintf(stderr, "loadFile: Out of memory error when trying to allocate cells\n");
       return NULL;
     }
   }
@@ -334,23 +464,43 @@ char **loadFile() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
-/* // this is a WIP; i was trying to improve the old getInput (perhaps needlessly), but i haven't tested this one yet... -EB
-int getInput(char *str, size_t *size) {
-
-  clearBuffer();
-
-  str = (char*)malloc(*size * sizeof(char));
-  if (str == NULL) {
-    fprintf(stderr, "ERROR: Out of memory\n");
+// Gets a line of input from the user.
+//
+// Takes a (NULL) char double-pointer (str) and a (0-value or NULL) size_t
+// pointer (size). *str will become a pointer to a null-delimited string (NOT
+// including the new-line character at the end), and size's value will become
+// the length of the string in chars (not including the null char).
+//
+// *str must be manually freed outside of the function.
+int getInput(char **str, size_t *size) {
+  bool discardSize = false;
+  if (size == NULL) {
+    size = (size_t *)malloc(sizeof(size_t));
+    *size = 0;
+    discardSize = true;
     return 0;
   }
 
-  getline(&str, size, stdin);
+  clearBuffer();
+
+  getline(str, size, stdin);
+  if (str == NULL) {
+    fprintf(stderr, "getInput: Out of memory\n");
+    return 0;
+  }
+
+  // remove the new-line char
+  (*str)[*size] = '\0';
+  *str = (char *)realloc(*str, (sizeof(char) * (*size)) + 1);
+
+  if (discardSize) { free(size); }
+
   return 1;
 }
-*/
 
+/*
 char *getInput(size_t bufsize) {
 
   fseek(stdin,0,SEEK_END);
@@ -368,7 +518,7 @@ char *getInput(size_t bufsize) {
     fgets(buffer, bufsize, stdin);
 
     return buffer;
-}
+}*/
 
 void clearBuffer() {
   char input;
--- cellAut.h
+++ cellAut.h
@@ -23,15 +23,39 @@ int printOutput();
 // calculated.
 int runAutomaton(bool printOutput);
 
+// Runs the Game of Life automaton using the current settings, iterating through
+// the specified number of generations, updating the 'output' array to each
+// generation in turn.
+int runGameOfLife(int generations);
+
+/*
+  This function attempts to open the given file in read or write mode.
+  The function takes in the file name and the mode and will return either
+  the pointer to the file or NULL if there was an error.
+*/
 FILE *attemptOpen(char* fileName, char* mode);
 
+/*
+  The following function saves the output array to a file and then returns 0 if
+  a error was encountered or 1 if it was successful.
+*/
 int saveFile();
 
+/*
+  The following function loads a file using attemptOpen(), then prompts the user
+  for a filename to open, and returns a 2D pointer array to what was found
+  within the file, or will return NULL if an error was encountered.
+
+  The function also changes the values of the global rows and columns variables
+  to match the new array.
+
+  The returned array must be freed manually (after each of its rows is freed).
+*/
 char **loadFile();
 
 // Gets input from the user
-//int getInput(char *str, size_t *size);
-char *getInput(size_t bufsize);
+int getInput(char **str, size_t *size);
+//int *getInput(size_t bufsize);
 
 // Clears the stdin buffer
 void clearBuffer();
